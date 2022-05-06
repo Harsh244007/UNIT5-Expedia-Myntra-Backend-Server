5a984276baf8e0e0348532a007080748106a0829
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-05-06 10:09:41 +0530

    main

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const strController = require("./controller/str.controller");
 app.use("/flight", flightController)
 app.use("/hotel", hotelController)
 app.use("/num", numController)
-app.use("/str",strController)
+app.use("/str", strController)
+
+app.get("/", (req, res)=>{
+    res.send("Hello It's Harsh unit5-expedia-backend")
+})
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const app = require("./index");
 const connect = require("./config/db");
-const port=5000;
-app.listen(port, async()=>{
+var server_host =  '0.0.0.0';
+app.listen(process.env.PORT || 5000,server_host, async()=>{
     try {
         await connect();
     } catch (error) {
         console.log(error.message);
     }
-    console.log("listening on "+port)
+    console.log("listening on ")
 });
