998cd351019e68c8258c32d29ec7ed7e5eba591e
Harsh Patel <97446580+Harsh244007@users.noreply.github.com>
2022-05-08 12:39:00 +0530

    main

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let cors = require('cors')
+let cors = require("cors");
 
-app.use(cors())
+const { register, login } = require("./controller/auth");
+const passport = require("./config/google-auth");
+
+app.use(cors());
 
 const flightController = require("./controller/flight.controller");
 const hotelController = require("./controller/hotel.controller");
@@ -10,14 +13,43 @@ const numController = require("./controller/num.controller");
 const strController = require("./controller/str.controller");
 const thingsController = require("./controller/things.controller");
 
-app.use("/flight", flightController)
-app.use("/hotel", hotelController)
-app.use("/num", numController)
-app.use("/str", strController)
-app.use("/things", thingsController)
+app.use("/flight", flightController);
+app.use("/hotel", hotelController);
+app.use("/num", numController);
+app.use("/str", strController);
+app.use("/things", thingsController);
+app.use("/login", login);
+app.use("/register", register);
 
-app.get("/", (req, res)=>{
-    res.send("Hello It's Harsh unit5-expedia-backend")
-})
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/google/failure",
+    successRedirect: "http://127.0.0.1:5500/home.html",
+  }),
+  (req, res) => {
+    const { user } = req;
+    const token = newToken(user);
+
+    return res.send({ user, token });
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello It's Harsh unit5-expedia-backend");
+});
 
 module.exports = app;
